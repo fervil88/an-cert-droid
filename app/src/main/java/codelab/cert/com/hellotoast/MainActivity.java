3f64d222cc7c971d4eda76735e16744bf74cf436
Fernando Villar <fernando.villar@windriver.com>
2019-09-13 16:05:20 -0300

    First Screen

---
+++ app/src/main/java/codelab/cert/com/hellotoast/MainActivity.java
@@ -0,0 +1,14 @@
+package codelab.cert.com.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //TODO 8. Task 6: Add onClick handlers for the buttons
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
