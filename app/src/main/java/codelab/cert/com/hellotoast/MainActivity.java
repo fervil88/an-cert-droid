1592ae87ea288bc95814a87768b5b04bafd831f3
fervil88 <fernandovillar88@gmail.com>
2019-09-15 21:18:33 -0300

    Added first code lab

--- app/src/main/java/codelab/cert/com/hellotoast/MainActivity.java
+++ app/src/main/java/codelab/cert/com/hellotoast/MainActivity.java
@@ -2,13 +2,35 @@ package codelab.cert.com.hellotoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //TODO 8. Task 6: Add onClick handlers for the buttons
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
